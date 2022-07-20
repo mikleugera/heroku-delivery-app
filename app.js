1bc476dcde013de5fe0a04b67a506e577dae4344
Mikle <44118053+mikleugera@users.noreply.github.com>
2022-07-20 14:19:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./routes/shops.routes")(app);
 require("./routes/shopping_card.routes")(app);
 
-const PORT = config.get('port') || 5000;
+const PORT = process.env.PORT || 5000)
 
 app.use(express.static(path.join(__dirname, 'build')));
 
