26a401002c3f9bb2c3ac04b88eba4f2d23c0427e
Mikle <44118053+mikleugera@users.noreply.github.com>
2022-07-20 14:56:57 +0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3');
 
 // open the database
-let db = new sqlite3.Database('../backend/Delivery_app_database.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./Delivery_app_database.sqlite', sqlite3.OPEN_READWRITE, (err) => {
    if (err) {
       console.error(err.message);
    }
