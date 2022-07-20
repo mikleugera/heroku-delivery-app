5a05a600db56889726517367d0ac48fea4812bf6
Mikle <44118053+mikleugera@users.noreply.github.com>
2022-07-20 20:59:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const PORT = config.get('port') || 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + './build/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'build', 'index.html'));
 });
 
 async function start(){
